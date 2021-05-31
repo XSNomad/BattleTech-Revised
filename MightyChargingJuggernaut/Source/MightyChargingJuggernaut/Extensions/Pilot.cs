7ad7e75c815745e6f15d3e753cb4fc83e2bf2ae0
XSNomad <60951221+XSNomad@users.noreply.github.com>
2021-05-30 20:08:44 -0400

    Added Juggernaut to Gladiator Tags

--- MightyChargingJuggernaut/Source/MightyChargingJuggernaut/Extensions/Pilot.cs
+++ MightyChargingJuggernaut/Source/MightyChargingJuggernaut/Extensions/Pilot.cs
@@ -10,6 +10,8 @@ namespace MightyChargingJuggernaut.Extensions
             var mechTags = pilot.ParentActor.GetTags();
             if (mechTags.Contains("BR_MQ_Charger"))
                 isJuggernaut = true;
+            if (pilot.pilotDef.PilotTags.Contains("pilot_gladiator"))
+                isJuggernaut = true;
             if (pilot.PassiveAbilities.Find((Ability a) => a.Def.Description.Name == "JUGGERNAUT") != null)
                 isJuggernaut = true;
             return isJuggernaut;
